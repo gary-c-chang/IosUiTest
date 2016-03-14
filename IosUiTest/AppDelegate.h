3ced95811472e8af74ea32d274b659f19c219971
Gary Chang <gary.c.chang@gmail.com>
2016-03-15 09:49:36 +1100

    Initial Commit

---
+++ IosUiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IosUiTest
+//
+//  Created by Gary Chang on 15/03/2016.
+//  Copyright © 2016 Gaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IosUiTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IosUiTest
+//
+//  Created by Gary Chang on 15/03/2016.
+//  Copyright © 2016 Gaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
